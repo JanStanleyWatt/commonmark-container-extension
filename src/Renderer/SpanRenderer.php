3c5522d9ca16e79dbac43967dd68a810c593e650
Jan Stanley Watt <github@mail.whojinn.com>
2023-03-15 02:25:22 +0900

    Add inline container parser, node, and renderer

---
+++ src/Renderer/SpanRenderer.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright 2023 Jan stanray watt
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *  http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace JSW\Container\Renderer;
+
+use JSW\Container\Node\Division;
+use JSW\Container\Node\Span;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Renderer\ChildNodeRendererInterface;
+use League\CommonMark\Renderer\NodeRendererInterface;
+use League\CommonMark\Util\HtmlElement;
+use League\CommonMark\Xml\XmlNodeRendererInterface;
+use League\Config\ConfigurationInterface;
+
+final class SpanRenderer implements NodeRendererInterface, XmlNodeRendererInterface
+{
+    private array $attrs = [];
+
+    /**
+     * @param Span $node
+     */
+    public function render(Node $node, ChildNodeRendererInterface $childRenderer)
+    {
+        Span::assertInstanceOf($node);
+        $this->attrs = $node->data->get('attributes');
+
+        return new HtmlElement('span', $this->attrs, $childRenderer->renderNodes($node->children()));
+    }
+
+    /**
+     * @param Division $node
+     */
+    public function getXmlTagName(Node $node): string
+    {
+        return 'span';
+    }
+
+    /**
+     * @param Division $node
+     */
+    public function getXmlAttributes(Node $node): array
+    {
+        return [] !== $this->attrs ? $this->attrs : [];
+    }
+}
